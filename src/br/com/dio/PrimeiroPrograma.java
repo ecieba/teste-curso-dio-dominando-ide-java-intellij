52b547a53be5513c0498770ad3f272c4a98c07fb
ecieba <ecieba@gmail.com>
2023-03-26 15:51:05 -0300

    criando class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,5 +6,19 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
         System.out.println(gato);
+
+        Livro livro = new Livro();
     }
+}
+
+class Livro {
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    private String nome;
 }
